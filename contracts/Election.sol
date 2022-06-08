5b83fd4c35dc44fce8a9d45172aead671a579d4e
Cristian Rosca <cristianrrosca@gmail.com>
2022-06-09 00:31:24 +0100

    add constructor, addCandidate function and test to ensure it initialises with 2 Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.4.2 <0.9.0;
 
 contract Election {
     string public candidate;
     
-    function election () public{
-        candidate = "Candidate 1";
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store candidates
+    // Fetch Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
