7df02be2fb8dcb5aed4445e12a2f92c31c3727c9
Cristian Rosca <cristianrrosca@gmail.com>
2022-06-09 11:26:10 +0100

    add test to ensure candidates initialise with correct values

--- test/election.js
+++ test/election.js
@@ -2,6 +2,8 @@ var Election = artifacts.require("./Election.sol");
 
 contract("Election", function(accounts) {
 
+    var Election = artifacts.require("./Election.sol");
+
     it("initialises with two candidates", function(){
         return Election.deployed().then(function(instance){
             return instance.candidatesCount();
@@ -9,5 +11,21 @@ contract("Election", function(accounts) {
             assert.equal(count, 2);
         })
     })
+
+    it("initializes the candidates with the correct values", function() {
+        return Election.deployed().then(function(instance) {
+            electionInstance = instance;
+            return electionInstance.candidates(1);
+        }).then(function(candidate) {
+            assert.equal(candidate[0], 1, "contains the correct id");
+            assert.equal(candidate[1], "Candidate 1", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct votes count");
+            return electionInstance.candidates(2);
+        }).then(function(candidate) {
+            assert.equal(candidate[0], 2, "contains the correct id");
+            assert.equal(candidate[1], "Candidate 2", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct votes count");
+        });
+    });
 
 })
